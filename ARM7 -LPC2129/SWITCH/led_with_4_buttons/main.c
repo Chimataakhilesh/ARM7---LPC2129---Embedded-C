75b9cff5bfc1cf922f1a52333ab7269b3f355276
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-01-26 00:14:32 +0530

    Update main.c

--- ARM7 -LPC2129/SWITCH/led_with_4_buttons/main.c
+++ ARM7 -LPC2129/SWITCH/led_with_4_buttons/main.c
@@ -1,1 +1,18 @@
+#include <LPC21XX.H>
+int main() {
+	unsigned int Switch; 	
+  IODIR0 = 0x0F000;
+    while (1) {
+        Switch = ((IOPIN0 >> 5) & (15));
+        switch (Switch){
+          case 0x0E: IOCLR0 = 0x01000; break;
+          case 0x0D: IOCLR0 = 0x02000; break;
+          case 0x0B: IOCLR0 = 0x04000; break;
+          case 0x07: IOCLR0 = 0x08000; break;
+          default: IOSET0 = 0x0F000;
+					break;
+        }
+    }
+}
+
 
