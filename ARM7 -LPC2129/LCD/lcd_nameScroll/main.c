857db388417b3e02fe572ef9bb81c28f3fa0bc60
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-01-26 00:39:18 +0530

    Update main.c

--- ARM7 -LPC2129/LCD/lcd_nameScroll/main.c
+++ ARM7 -LPC2129/LCD/lcd_nameScroll/main.c
@@ -1,1 +1,42 @@
+#include<LPC21XX.H>
+#include "lcd_header.h"
+unsigned char display_text[]="EMBEDDED SYSTEM ";
+void LCD_NameScroll(unsigned char *);
+void scroll_right(unsigned char *);
+void scroll_left(unsigned char *);
+int main()
+{
+	LCD_INIT();
+	while(1){
+		LCD_NameScroll(display_text);
+		scroll_left(display_text);
+		delay_ms(20);
+		/*scroll_right(display_text);
+		delay_ms(100);*/
+	}
+}
+void LCD_NameScroll(unsigned char *s)
+{
+	LCD_COMMAND(0x80);
+	while(*s)
+	{
+		LCD_DATA(*s++);
+	}
+}
+void scroll_right(unsigned char *arr)
+{
+	int i;
+	char last = arr[15];
+	for(i=15;i>0;i--)
+	arr[i]=arr[i-1];
+	arr[0] = last; 
+}
+void scroll_left(unsigned char *arr)
+{
+	int i;
+	char first = arr[0];
+	for(i=0;i<15;i++)
+	arr[i]=arr[i+1];
+	arr[15] = first;
+}
 
