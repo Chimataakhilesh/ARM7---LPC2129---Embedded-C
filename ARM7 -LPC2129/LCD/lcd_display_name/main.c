75f929fa9fb45dfae884fd29fa5fab647fa03a6d
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-01-26 00:32:09 +0530

    Update main.c

--- ARM7 -LPC2129/LCD/lcd_display_name/main.c
+++ ARM7 -LPC2129/LCD/lcd_display_name/main.c
@@ -1,1 +1,20 @@
+#include<LPC21XX.H>
+#include "lcd_header.h"
 
+void LCD_STRING(unsigned char *);
+int main(){
+	LCD_INIT();
+	LCD_COMMAND(0x80);
+	LCD_STRING("CHIMATA AKHILESH");
+}
+void LCD_STRING(unsigned char *s)
+{
+	char i=0;
+	while(*s){
+		LCD_DATA(*s++);
+		i++;
+		if(i==16)
+		LCD_COMMAND(0xc0);
+
+}
+}
